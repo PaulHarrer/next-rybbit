160ce448a6484575759b5d06c817e2e6cdc4e54e
Paul Harrer <16596572+PaulHarrer@users.noreply.github.com>
2025-06-01 21:17:56 +0200

    Patch: updated RybbitProvider to fix hydration error

--- src/RybbitProvider.tsx
+++ src/RybbitProvider.tsx
@@ -2,74 +2,41 @@ import React from "react";
 import Script from "next/script";
 import { RybbitProps } from "./types";
 
-const RybbitProvider: React.FC<RybbitProps> = ({
-  analyticsHost = "https://api.rybbit.io/api",
-  siteId,
-  enabled = true,
-  trackLocalhost = false,
-  debounce = 500,
-  autoTrackPageviews = true,
-  autoTrackSpaRoutes = true,
-  trackQuerystring = true,
-  trackOutboundLinks = true,
-  skipPatterns = [],
-  maskPatterns = [],
-  debug = false,
-  integrity,
-  src,
-}) => {
-  // Skip rendering if disabled
-  if (!enabled) {
-    return null;
-  }
+const RybbitProvider: React.FC<RybbitProps> = (props) => {
+  const config = React.useMemo(() => {
+    return {
+      analyticsHost: props.analyticsHost || "https://api.rybbit.io/api",
+      siteId: props.siteId.toString()
+    };
+  }, [props]);
 
-  // Skip localhost tracking if not enabled
-  if (!trackLocalhost && typeof window !== "undefined" && window.location.hostname === "localhost") {
-    return null;
-  }
-
-  // Build script source URL
-  const getScriptSrc = (): string => {
-    if (src) return src;
-
-    // analyticsHost should include the full path like https://rybbit.yourdomain.com/api
-    const baseUrl = analyticsHost.replace(/\/api\/?$/, ""); // Remove /api if present
+  const scriptSrc = React.useMemo(() => {
+    if (props.src) return props.src;
+    const baseUrl = (props.analyticsHost || "https://api.rybbit.io/api").replace(/\/api\/?$/, "");
     return `${baseUrl}/api/script.js`;
-  };
-
-  const scriptSrc = getScriptSrc();
-
-  // Build configuration object
-  const config = {
-    analyticsHost,
-    siteId: siteId.toString(),
-    debounce,
-    autoTrackPageviews,
-    autoTrackSpaRoutes,
-    trackQuerystring,
-    trackOutboundLinks,
-    skipPatterns,
-    maskPatterns,
-    debug,
-  };
+  }, [props.src, props.analyticsHost]);
 
   return (
-    <>
-      {/* Initialize rybbit config before script loads */}
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `window.rybbitConfig = ${JSON.stringify(config)};`
-        }}
-      />
-      <Script
-        src={scriptSrc}
-        data-site-id={siteId}
-        strategy="afterInteractive"
-        integrity={integrity}
-        crossOrigin={integrity ? "anonymous" : undefined}
-        async
-      />
-    </>
+    <div style={{ display: 'contents' }}>
+      {typeof window !== 'undefined' &&
+        !(props.trackLocalhost === false && window.location.hostname === "localhost") && (
+        <>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.rybbitConfig = ${JSON.stringify(config)};`
+            }}
+          />
+          <Script
+            src={scriptSrc}
+            data-site-id={props.siteId}
+            strategy="afterInteractive"
+            integrity={props.integrity}
+            crossOrigin={props.integrity ? "anonymous" : undefined}
+            async
+          />
+        </>
+      )}
+    </div>
   );
 };
 
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 export { default as RybbitProvider } from "./RybbitProvider";
 export { useRybbit } from "./useRybbit";
 export type {
-  RybbitOptions,
   RybbitProps,
   EventOptions,
   PageviewOptions,
