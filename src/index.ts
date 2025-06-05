ab30808e52782c9871d6f3c15ba133ac38235488
Paul Harrer <16596572+PaulHarrer@users.noreply.github.com>
2025-06-05 20:13:18 +0200

    Patch: added events

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { RybbitProvider, default } from "./RybbitProvider";
 export type { RybbitProviderProps } from "./RybbitProvider";
+export { useRybbit } from "./useRybbit";
+export type { EventOptions, PageviewOptions } from "./types";
