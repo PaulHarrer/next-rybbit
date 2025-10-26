d4a8a89b33490d37636a0f038b34af733b87cead
Paul Harrer <16596572+PaulHarrer@users.noreply.github.com>
2025-10-26 10:39:08 +0100

    Patch: updated script attributes

--- src/RybbitProvider.tsx
+++ src/RybbitProvider.tsx
@@ -2,34 +2,24 @@ import React from 'react';
 import Script from 'next/script';
 
 export interface RybbitProviderProps {
-  siteId: string;
-  analyticsHost?: string;
-  autoTrackPageview?: string;
-  trackSpa?: string;
-  trackQuery?: string;
-  trackOutbound?: string;
-  trackErrors?: string;
-  sessionReplay?: string;
-  webVitals?: string;
-  skipPatterns?: string[];
-  maskPatterns?: string[];
-  debounce?: string;
-  apiKey?: string;
-  children: React.ReactNode;
+    analyticsHost?: string;
+    siteId: string;
+    debounce?: string;
+    webVitals?: string;
+    skipPatterns?: string[];
+    maskPatterns?: string[];
+    replayMaskTextSelectors?: string[];
+    apiKey?: string;
+    children: React.ReactNode;
 }
 
 export const RybbitProvider: React.FC<RybbitProviderProps> = ({
   siteId,
   analyticsHost = 'https://app.rybbit.io/api',
-  autoTrackPageview = 'true',
-  trackSpa = 'true',
-  trackQuery = 'true',
-  trackOutbound = 'true',
-  trackErrors = 'false',
-  sessionReplay = 'false',
   webVitals = 'false',
   skipPatterns = [],
   maskPatterns = [],
+  replayMaskTextSelectors = [],
   debounce = '500',
   apiKey = undefined,
   children
@@ -42,15 +32,10 @@ export const RybbitProvider: React.FC<RybbitProviderProps> = ({
       <Script
         src={scriptSrc}
         data-site-id={siteId}
-        data-auto-track-pageview={autoTrackPageview}
-        data-track-spa={trackSpa}
-        data-query={trackQuery}
-        data-track-outbound={trackOutbound}
-        data-track-errors={trackErrors}
-        data-session-replay={sessionReplay}
         data-web-vitals={webVitals}
         data-skip-patterns={skipPatterns}
         data-mask-patterns={maskPatterns}
+        data-replay-mask-text-selectors={replayMaskTextSelectors}
         data-debounce={debounce}
         data-api-key={apiKey}
         strategy="afterInteractive"
